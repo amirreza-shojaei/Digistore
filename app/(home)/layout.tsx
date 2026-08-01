8083b25c657cde9a0d22c74d1429ab88ae6a4450
AmirrezaShojaei <AmirrezaShojaei@example.com>
2026-08-01 16:13:50 +0330

    create project

---
+++ app/(home)/layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+interface HLayout{
+    children:React.ReactNode;
+}
+function Layout({children}:HLayout) {
+  return (
+    <div>{children}</div>
+  )
+}
+
+export default Layout
